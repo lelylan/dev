cca4a699581c9d0741a5b47a329e07c68d53d282
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-28 15:55:49 +0200

    Added all views defined in nanoc but with erb extension

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 #require 'rails/all'
 require 'action_controller/railtie'
+require 'sprockets/railtie'
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
