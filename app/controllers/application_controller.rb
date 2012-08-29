aea6239c647b6abb80ae8d60fce0564ae74c4476
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-29 14:21:24 +0200

    Added select box to choose the actual language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :body_style, :title, :index_page?
+  helper_method :body_style, :title, :index_page?, :code_page?
 
   def body_style
     index_page? ? 'index' : 'description'
   end
 
+  def title
+    Settings.titles[params[:id]] || 'Develop the connected house'
+  end
+
   def index_page?
     Settings.index_pages.include?(params[:id])
   end
-
-  def title
-    Settings.titles[params[:id]] || 'Develop the connected house'
+  
+  def code_page?
+    Settings.code_pages.include?(params[:id])
   end
 end
