805aede9d623a951509a2de87a4d2ffefee0fce4
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-29 17:17:32 +0200

    Added system to set the language with fragment and select. Also direct link to the example code

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,102 +1,49 @@
+// code highlight
 $(function () { hljs.initHighlightingOnLoad(); })
 
-var Site = {}
-Site.animation = (function() {
+// language examples selection (ruby, js, curl, tc.)
+$(function () {
 
-  var me = {};
+  set_language_from_fragment()
+  set_language(find_or_create_language());
 
-  me.init = function(container_id) {
+  // Change language through the select
+  $('#language').change(function () {
+    var language = $("#language option:selected").first().attr('value');
+    set_language(language);
+  })
 
-    // Page structure 
-    var container = $(container_id),
-        canvas    = container.find('.canvas'),
-        paper     = Raphael(canvas.attr('id'));
-
-    // Lines visualization settings
-    var path_attr   = {"stroke-width": 2, stroke: "#ddd"},
-        first_line  = paper.path("M18 20L292 20").attr(path_attr),
-        second_line = paper.path("M308 20L610 20").attr(path_attr);
-
-    // Circle visualization settings
-    var circle_attr   = {stroke: "none", fill: "#ddd"},
-        first_circle  = paper.circle(15, 20, 10).attr(circle_attr);
-        second_circle = paper.circle(300, 20, 10).attr(circle_attr);
-        third_circle  = paper.circle(615, 20, 10).attr(circle_attr);
-
-    // Moving circle visualization settings
-    var moving_circle_attr = {stroke: "#FF7600", "stroke-width": 4},
-        moving_circle      = paper.circle(15, 20, 4).attr(moving_circle_attr);
-
-    // Apply a bouncing animation to the first circle
-    setTimeout(fade(first_circle, container.find('.first-step')), 1000);
-
-    // Show the first stem description
-    show_step('.first-step');
-
-    // Buttons click
-    container.find(".first-step .btn-warning").click(function (e)  { go_to_second_step(e) })
-    container.find(".second-step .btn-warning").click(function (e) { go_to_third_step(e) })
-    container.find(".third-step .btn-warning").click(function (e)  { go_to_first_step(e) })
-
-    first_circle.click(function (e) { go_to_first_step(e) })
-    second_circle.click(function (e) { go_to_second_step(e) })
-    third_circle.click(function (e) { go_to_third_step(e) })
-
-    // ----------
-    // Private
-    // ----------
-
-      // Node arrival animation
-      //   - circle: object to animate at the end of the animation
-      //   - content_id: step description to show
-      function fade(circle, step) {
-        return function () {
-          show_step(step);
-          circle.attr({fill: "#FF7600", r: 14}).animate({fill: "#ddd", r: 10, easing: '>'}, 1000);
-        };
-      }
-
-      // Create animation
-      //   - x: final x position
-      //   - circle: object to animate at the end of the animation
-      //   - content_id: step description to show
-      function animate(x, circle, content_id) {
-        moving_circle.stop().animate({"100%": {cx: x, easing: '<', callback: fade(circle, content_id)}});
-      }
-
-      // Visible step description
-      //   - step: id related to the step description to show
-      function show_step(step) {
-        container.find('.first-step').hide();
-        container.find('.second-step').hide();
-        container.find('.third-step').hide();
-        container.find(step).show();
-      }
-
-      // Animation to first position
-      function go_to_first_step(e) {
-        moving_circle.attr({cx: 15});
-        setTimeout(fade(first_circle, '.first-step'), 0);
-        e.preventDefault();       
-      }
-
-      // Animation to second position
-      function go_to_second_step(e) {
-        moving_circle.attr({cx: 15});
-        animate(300, second_circle, '.second-step', 0);
-        e.preventDefault();
-      }
-
-      // Animation to third position
-      function go_to_third_step(e) {
-        moving_circle.attr({cx: 300});
-        animate(615, third_circle, '.third-step', 0);
-        e.preventDefault();
-      }
-
+  // Retrieve the language set on the cookies or set the new one
+  function find_or_create_language() {
+    var language = $.cookie('lelylan_dev_language');
+    if (language == null) { $.cookie('lelylan_dev_language', 'curl'); }
+    return $.cookie('lelylan_dev_language');
   }
 
-  return me;
-})();
+  // Viasualize the documentation through the selected language
+  function set_language(language) {
+    // show all code examples
+    $('#language').val(language);
+    // set the select menu
+    $('.nav-tabs a.' + language).tab('show');
+    // set the language as the default one
+    $.cookie('lelylan_dev_language', language);
+  }
 
+  // If there is the name of the language in the fragment it is set as default
+  function set_language_from_fragment() {
+    var languages = ['curl', 'javascript', 'ruby'];
+    for (var i=0; i < languages.length; i++) {
+      language = languages[i];
+      console.log(fragment_contains(language), language)
+      if (fragment_contains(language)) {
+        $.cookie('lelylan_dev_language', language);
+      }
+    }
+  }
 
+  // return true if a language is into the fragment
+  function fragment_contains(language) {
+    return (window.location.hash.indexOf(language) != -1)
+  }
+});
