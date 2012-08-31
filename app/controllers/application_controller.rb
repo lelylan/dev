25744182ba03372ce9c67daef1f4629c3146efc7
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-31 12:49:19 +0200

    Signout now point to people service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :body_style, :title, :index_page?, :code_page?
+  helper_method :body_style, :title, :index_page?, :code_page?, :people_options, :people_host
 
   def body_style
     index_page? ? 'index' : 'description'
@@ -14,8 +14,18 @@ class ApplicationController < ActionController::Base
   def index_page?
     Settings.index_pages.include?(params[:id])
   end
-  
+
   def code_page?
     Settings.code_pages.include?(params[:id])
+  end
+
+  def people_options(options = { host: 'localhost', port: 80 })
+    options[:host] = ENV['LELYLAN_PEOPLE_URL']  if ENV['LELYLAN_PEOPLE_URL']
+    options[:port] = ENV['LELYLAN_PEOPLE_PORT'] if ENV['LELYLAN_PEOPLE_PORT']
+    options
+  end
+
+  def people_host
+    "#{request.protocol}#{ ENV['LELYLAN_PEOPLE_URL']}:#{ENV['LELYLAN_PEOPLE_PORT']}"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,15 +11,4 @@ module ApplicationHelper
   def resource_class
     devise_mapping.to
   end
-
-  def people_options(options = { host: 'localhost' })
-    options[:host] = ENV['LELYLAN_PEOPLE_URL']  if ENV['LELYLAN_PEOPLE_URL']
-    options[:port] = ENV['LELYLAN_PEOPLE_PORT'] if ENV['LELYLAN_PEOPLE_PORT']
-    puts options
-    options
-  end
-
-  def people_host
-    "#{request.protocol}#{ ENV['LELYLAN_PEOPLE_URL']}:#{ENV['LELYLAN_PEOPLE_PORT']}"
-  end
 end
