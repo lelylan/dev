eee26a6700591f182c0a1c57cd5b3055df1def06
Andrea Reginato <andrea.reginato@gmail.com>
2012-12-19 20:12:33 +0100

    minor cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,5 @@ Dev::Application.configure do
   config.session_store :cookie_store, key: '_dev_session', domain: ENV['SESSION_STORE_DOMAIN']
 
   # CDN configuration
-  config.action_controller.asset_host = "http://#{ENV['FOG_CLOUDFRONT']}.cloudfront.net"
-  #config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_CLOUDFRONT']}.cloudfront.net"
 end
