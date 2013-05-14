760704bfbc7e9fe93a1e759f47e9db891acf633b
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-14 17:09:32 +0200

    Added 'apps' page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   layout 'basic'
 
-        def after_sign_out_path_for(resource_or_scope)
-        people_host
-      end
+  def after_sign_out_path_for(resource_or_scope)
+    people_host
+  end
 end
