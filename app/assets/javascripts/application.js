184c21e083c9bfb686dfed225343366d47b65ee0
Andrea Reginato <andrea.reginato@gmail.com>
2013-09-24 13:02:22 +0200

    Fixed representation anchor and code blocks positioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,27 +15,26 @@
 //= require_tree .
 //
 
-// pretty print
+/* pretty print */
+
 addEventListener('load', function (event) { prettyPrint() }, false);
 
-// open all external links in new tab
+/* open all external links in new tab */
 
 $(function(){
   $('a').each(function() {
     var a = new RegExp('/' + window.location.host + '/');
     if(!a.test(this.href)) {
-        $(this).click(function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            window.open(this.href, '_blank');
-        });
+      $(this).click(function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        window.open(this.href, '_blank');
+      });
     }
   });
 });
 
-// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
-// IT'S ALL JUST JUNK FOR OUR DOCS!
-// ++++++++++++++++++++++++++++++++++++++++++
+/* Left menu affix */
 
 !function ($) {
 
@@ -79,41 +78,6 @@ $(function(){
       })
     }, 100)
 
-    setTimeout(function () {
-      $('.bs-top').affix()
-    }, 100)
-
-    // tooltip demo
-    $('.tooltip-demo').tooltip({
-      selector: "[data-toggle=tooltip]",
-      container: "body"
-    })
-
-    $('.tooltip-test').tooltip()
-    $('.popover-test').popover()
-
-    $('.bs-docs-navbar').tooltip({
-      selector: "a[data-toggle=tooltip]",
-      container: ".bs-docs-navbar .nav"
-    })
-
-    // popover demo
-    $("[data-toggle=popover]")
-      .popover()
-
-    // button state demo
-    $('#fat-btn')
-      .click(function () {
-        var btn = $(this)
-        btn.button('loading')
-        setTimeout(function () {
-          btn.button('reset')
-        }, 3000)
-      })
-
-    // carousel demo
-    $('.bs-docs-carousel-example').carousel()
-})
-
+  })
 }(window.jQuery)
 
